0c827192d70d5a8740c2e6f2b4d22bd7bb946c31
Piyush Singh <94911177+singhpiyush55@users.noreply.github.com>
2026-06-12 14:13:17 +0530

    update prisma.ts as new generated path

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,27 @@
-import { PrismaClient } from "../generated/prisma";
-import { PrismaNeon } from "@prisma/adapter-neon";
+// import { PrismaClient } from "../generated/prisma";
+// import { PrismaNeon } from "@prisma/adapter-neon";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+// const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-function createPrismaClient() {
-  const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL! });
-  return new PrismaClient({ adapter });
+// function createPrismaClient() {
+//   const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL! });
+//   return new PrismaClient({ adapter });
+// }
+
+// export const prisma = globalForPrisma.prisma ?? createPrismaClient();
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
 }
-
-export const prisma = globalForPrisma.prisma ?? createPrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
