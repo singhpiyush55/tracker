22cfbcf04da257b0e9a536e8d4b45cd996256c84
Piyush Singh <94911177+singhpiyush55@users.noreply.github.com>
2026-06-12 13:09:53 +0530

    updated read

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
-export { default } from "next-auth/middleware";
+  export { default } from "next-auth/middleware";
 
-export const config = {
-  matcher: [
-    "/dashboard/:path*",
-    "/screener/:path*",
-    "/trades/:path*",
-  ],
-};
+  export const config = {
+    matcher: [
+      "/dashboard/:path*",
+      "/screener/:path*",
+      "/trades/:path*",
+    ],
+  };
